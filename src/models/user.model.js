d6cb765ea19e829a4886801d34727a55f20141f9
Duong <phduongse@gmail.com>
2019-11-04 16:01:09 +0700

    circle

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,110 +1,121 @@
 export default function(sequelize, DataTypes) {
     const user = sequelize.define(
-        "user", // Model Name
+        'user', // Model Name
         {
             phoneNumber: {
                 type: DataTypes.STRING,
                 allowNull: false,
-                unique: true
+                unique: true,
             },
             email: {
                 type: DataTypes.STRING,
                 allowNull: false,
-                unique: true
+                unique: true,
             },
             password: {
                 type: DataTypes.STRING,
-                allowNull: false
+                allowNull: false,
             },
             nickname: {
                 type: DataTypes.STRING,
-                allowNull: false
+                allowNull: false,
             },
             gender: {
-                type: DataTypes.ENUM("MALE", "FEMALE")
+                type: DataTypes.ENUM('MALE', 'FEMALE'),
                 // allowNull: false,
             },
             dateOfBirth: {
-                type: DataTypes.DATEONLY
+                type: DataTypes.DATEONLY,
                 // allowNull: false,
             },
             address: {
                 type: DataTypes.STRING,
-                allowNull: false
-            }
+                allowNull: false,
+            },
         },
         {
             timestamps: true,
-            charset: "utf8",
-            collate: "utf8_general_ci"
-        }
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+        },
     );
 
     user.associate = function(models) {
         // user - parent
         user.hasOne(models.parent, {
-            foreignKey: "userId",
-            as: "parent",
-            onDelete: "CASCADE"
+            foreignKey: 'userId',
+            as: 'parent',
+            onDelete: 'CASCADE',
         });
 
         // user - babysitter
         user.hasOne(models.babysitter, {
-            foreignKey: "userId",
-            as: "babysitter",
-            onDelete: "CASCADE"
+            foreignKey: 'userId',
+            as: 'babysitter',
+            onDelete: 'CASCADE',
         });
 
         // user - sittingRequest
         user.hasMany(models.sittingRequest, {
             foreignKey: {
-                name: "createdUser",
-                allowNull: false
+                name: 'createdUser',
+                allowNull: false,
             },
-            sourceKey: "id",
-            as: "sittingRequests",
-            onDelete: "CASCADE"
+            sourceKey: 'id',
+            as: 'sittingRequests',
+            onDelete: 'CASCADE',
+        });
+
+        // user - sittingRequest
+        user.hasMany(models.sittingRequest, {
+            foreignKey: {
+                name: 'acceptedBabysitter',
+                allowNull: false,
+            },
+            sourceKey: 'id',
+            as: 'requests',
+            onDelete: 'CASCADE',
         });
 
         // user - invitation
         user.hasMany(models.invitation, {
             foreignKey: {
-                name: "receiver",
-                allowNull: false
+                name: 'receiver',
+                allowNull: false,
             },
-            sourceKey: "id",
-            as: "invitations"
+            sourceKey: 'id',
+            as: 'invitations',
         });
 
         // user - transaction
         user.hasMany(models.transaction, {
             foreignKey: {
-                name: "userId",
-                allowNull: false
+                name: 'userId',
+                allowNull: false,
             },
-            sourceKey: "id",
-            as: "transactions"
+            sourceKey: 'id',
+            as: 'transactions',
         });
 
         // user - tracking
         user.hasOne(models.tracking, {
             foreignKey: {
-                name: "userId",
-                allowNull: false
+                name: 'userId',
+                allowNull: false,
             },
-            sourceKey: "id",
-            as: "tracking"
-        })
+            sourceKey: 'id',
+            as: 'tracking',
+        });
 
         // user - schedule
         user.hasMany(models.schedule, {
             foreignKey: {
-                name: "userId",
-                allowNull: false
+                name: 'userId',
+                allowNull: false,
             },
-            sourceKey: "id",
-            as: "schedules",
-            onDelete: "CASCADE"
+            sourceKey: 'id',
+            as: 'schedules',
+            onDelete: 'CASCADE',
         });
     };
 
---
+++ src/services/circleService.js
@@ -0,0 +1,108 @@
+import models from '@models';
+import Sequelize from 'sequelize';
+
+export async function getCircleDetail(ownerId) {
+    let circle = await getCircle(ownerId);
+    let hiredSitter = await findHiredSitters(ownerId);
+    let friendSitter = await findFriendSittersInCircle(circle);
+
+    return {circle: circle, hiredSitter: hiredSitter, friendSitter: friendSitter};
+}
+
+async function getCircle(ownerId) {
+    let circle = await models.circle.findAll({
+        where: {
+            ownerId: ownerId,
+        },
+        include: [
+            {
+                model: models.parent,
+                as: 'friend',
+                include: [
+                    {
+                        model: models.user,
+                        as: 'user',
+                    },
+                ],
+            },
+        ],
+    });
+
+    return circle;
+}
+
+async function findHiredSitters(userId) {
+    let hiredIds = await models.sittingRequest.findAll({
+        attributes: [[Sequelize.fn('DISTINCT', Sequelize.col('acceptedBabysitter')), 'acceptedBabysitter']],
+        where: {
+            createdUser: userId,
+        },
+        // include: {
+        //     model: models.user,
+        //     as: 'bsitter',
+        //     include: {
+        //         model: models.babysitter,
+        //         as: 'babysitter',
+        //     }
+        // }
+    });
+
+    hiredIds = hiredIds.map(id => { return id.acceptedBabysitter});
+
+    let hireds = await models.babysitter.findAll({
+        where: {
+            userId: {
+                [Sequelize.Op.or]: hiredIds,
+            }
+        },
+        include: {
+            model: models.user,
+            as: 'user',
+        }
+    });
+
+    return hireds;
+}
+
+async function findFriendSittersInCircle(circle) {
+    if (circle.length <= 0) {
+        return null;
+    }
+    let friends = circle.map(record => {
+        return record.friendId;
+    })
+    let friendSitterIds = await models.sittingRequest.findAll({
+        attributes: [[Sequelize.fn('DISTINCT', Sequelize.col('acceptedBabysitter')), 'acceptedBabysitter']],
+        where: {
+            createdUser: {
+                [Sequelize.Op.or]: friends,
+            },
+        },
+        // include: {
+        //     model: models.user,
+        //     as: 'user',
+        //     include: {
+        //         model: models.babysitter,
+        //         as: 'babysitter',
+        //     }
+        // }
+    });
+
+    friendSitterIds = friendSitterIds.map(id => {return id.acceptedBabysitter});
+    
+    let friendSitter = await models.babysitter.findAll({
+        
+        where: {
+            userId: {
+                [Sequelize.Op.or]: friendSitterIds
+            }
+        },
+        include: {
+            model: models.user,
+            as: 'user',
+            attributes: {exclude: ['password']},
+        }
+    });
+        
+    return friendSitter;
+}
